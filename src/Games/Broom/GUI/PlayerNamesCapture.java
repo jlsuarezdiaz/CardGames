788c18eca976dc519f34029fc9880fbfb8342b2f
Javier <Javier@192.168.1.39>
2015-09-11 12:52:57 +0200

    Subiendo la escoba entera y funcionando

--- src/Games/Broom/GUI/PlayerNamesCapture.java
+++ src/Games/Broom/GUI/PlayerNamesCapture.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI;
+package Games.Broom.GUI;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
